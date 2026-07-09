bad3e4e050f873c29e930b573fae29afe3de9877
Thegood2006 <jpaucar@estud.tesa.edu.ec>
2026-07-08 19:11:34 -0500

    Admin: update server.js

--- server.js
+++ server.js
@@ -92,15 +92,7 @@ const CANALES_SERVER = {
       fuente: "manual"
     }
   },
-  "tctelevision": {
-    nombre: "TC Televisión",
-    url: "https://tctelevision.com/envivo/",
-    embedFijo: {
-      tipo:  "m3u8",
-      embed: "https://cdndirector.dailymotion.com/cdn/live/video/x7wijay.m3u8?sec=Y8K0tZvpDxl3qXdtgkbk9yy9cmkrwWyABOt2lbX7WvoJmKvhcWnu8VDJBCv4dFPdp-iPfTRDDKkr3UcUbTjoAQwaqtxjGA-czN4J9yJBCi0&dmTs=71389&dmV1st=16923b6e-b4e3-1b3f-2b27-5e2f2e41e8ca",
-      fuente: "manual"
-    }
-  },
+
   "tvc": {
     nombre: "TVC Ecuador",
     url: "https://www.tvc.com.ec/envivo/",
